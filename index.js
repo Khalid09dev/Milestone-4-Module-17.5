38ab5bfbd444257e307c1b9f3f518ff3b9bec97b
khalid09dev <mdkhalidhossen@hotmail.com>
2024-02-04 13:30:56 +0600

    added index.js and length.js file

---
+++ index.js
@@ -0,0 +1,15 @@
+const numbers = [77, 23, 22, 11, 19, 43];
+
+// get element value by index
+// console.log(numbers);
+console.log(numbers[4]);
+
+const fourth = numbers[3];
+console.log(fourth);
+
+// set or update element value by index
+let abc = 45;
+abc = 99;
+
+numbers[1] = 999;
+console.log(numbers);
---
+++ length.js
@@ -0,0 +1,2 @@
+const numbers = [1, 2, 5, 9, 12, 93];
+console.log(numbers.length);
