3d75528025612dda8c92b6341cc78c3debfd2bdc
khalid09dev <mdkhalidhossen@hotmail.com>
2024-02-04 12:30:43 +0600

    first commit

---
+++ array.js
@@ -0,0 +1,25 @@
+// const num1 = 12;
+// const num2 = 23;
+// const num3 = 443;
+// const num4 = 1212;
+
+// const numbers = 2342342345123423;
+
+//(first braket / parenthesis), {second braket / curly brakets}, [third braket / square braket]
+
+// const numbers = [12, 22]
+
+// Array ==> enables storing a collection of multiple items under a single variable name.
+// let's say you need 1-10 numbers for a code. Will you take 10 variables to store 10 values?
+
+// var num1 = 1;
+// var num2 = 2;
+// var num3 = 3;
+
+// that's why arrays are needed!
+// var numArray = [1,2,3,4,5,6,7,8,9,10]; ==> this is an array.
+
+const numbers = [12, 22, 54, 65];
+const friends = ['abul', 'babul', 'kabul', 'dabul', 'bulbul'];
+const passed = [true, false, true, false];
+const mixedArray = [12, 'Alahbad', true, 45.566];
